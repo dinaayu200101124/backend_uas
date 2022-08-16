9feb0b40628d9e61b5a438de55d24ffc0addcd2e
Dina Ayu <202030058@mhs.udb.ac.id>
2022-08-16 12:54:09 +0700

    merubah

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
- 
+
 const app = express();
-mongoose.connect('mongodb+srv://ayu:ayu@ujian.sajnktw.mongodb.net/?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect("mongodb+srv://ayu:ayu@ujian.sajnktw.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-db.on('error', (error) => console.log(error));
-db.once('open', () => console.log('Database Connected...'));
- 
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Database Connected..."));
+
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
- 
-app.listen(process.env.PORT || 5000, ()=> console.log('Server up and running...'));
+
+app.listen(process.env.PORT || 5000, () => console.log("Server up and running..."));
